f2b47d6667a8ebf7c56e469c0331fc762efaf676
Leys Yam <jam.leisan@yandex.ru>
2025-11-03 18:49:33 +0300

    Complete Day_1

---
+++ Day_1/task_1.py
@@ -0,0 +1,2 @@
+for i in list((10, 9.8, 3.14,4 - 4j, ['Asabeneh', 'Python', 'Finland'],'Your name','Your family name', 'Your country')):
+    print (type(i))
---
+++ Day_1/task_2_distance.py
@@ -0,0 +1,7 @@
+import math
+x1, y1 = 2, 3
+x2, y2 = 10, 8
+
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+print(distance)
