e66b5873a28d50ed850cb800f4c27e0a12359e6e
Leys Yam <jam.leisan@yandex.ru>
2026-06-23 16:24:41 +0300

    Day_15

---
+++ Errors/Types_error.py
@@ -0,0 +1,15 @@
+import math #maths - Module Error
+#math.PI (AttributeError)
+
+users = {'name':'Asab', 'age':250, 'country':'Finland'}
+#print(users['county']) - Key Error
+
+#print (4+'4') - Type Error -> fix: 4 + int('3')
+
+#from math import power -> fix:pow - print (pow(2,3))
+
+#int('12a'), int("25.5") - Value Error
+
+#1/0 - ZeroDivisionError
+
+
